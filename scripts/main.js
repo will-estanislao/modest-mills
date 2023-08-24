3cf6f57d475aef4a581386d4f0862b3cf0dd5163
Will Estanislao <72217283+estanislaowillia@users.noreply.github.com>
2023-08-24 17:56:30 -0400

    Updated image carousel

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+/**
+ * 24/08/23
+ */
+
+const body = document.body;
+const leftButton = body.getElementById("left-btn");
+const rightButton = body.getElementById("right-btn");
+
+/**
+ * Functions
+ */
+
+// Moves the images in the carousel
+// Determine the button clicked
+// Depending on which button is clicked move
+function moveImageCarousel(elementId) {
+  if (elementId == "left-btn") {
+    // Move carousel to left
+  } else if (elementId == "right-btn") {
+    // Move carousel to right
+  }
+}
+
+/**
+ * Assign Events
+ */
