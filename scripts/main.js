bc8e4916f4ff15b73c0e60be81e33cc6c70a6ea4
Will Estanislao <72217283+estanislaowillia@users.noreply.github.com>
2023-08-28 12:10:41 -0400

    Added image scrolling function to image carousel

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,22 @@
  */
 
 const body = document.body;
-const leftButton = body.getElementById("left-btn");
-const rightButton = body.getElementById("right-btn");
+const imageSet = body.getElementsByClassName("carousel-img");
+const leftButton = document.getElementById("left-btn");
+const rightButton = document.getElementById("right-btn");
+
+/**
+ * Assign Events
+ */
+leftButton.addEventListener("click", (e) => {
+  console.log("click Left");
+  moveRight();
+});
+
+rightButton.addEventListener("click", (e) => {
+  console.log("click Right");
+  moveLeft();
+});
 
 /**
  * Functions
@@ -13,14 +27,24 @@ const rightButton = body.getElementById("right-btn");
 // Moves the images in the carousel
 // Determine the button clicked
 // Depending on which button is clicked move
-function moveImageCarousel(elementId) {
-  if (elementId == "left-btn") {
-    // Move carousel to left
-  } else if (elementId == "right-btn") {
-    // Move carousel to right
+function moveLeft() {
+  for (i = 0; i < imageSet.length; i++) {
+    imageSet.item(i).classList.remove("carousel-img-right");
+    imageSet.item(i).classList.add("carousel-img-left");
+    checkImageClassList(imageSet.item(i));
   }
 }
 
-/**
- * Assign Events
- */
+function moveRight() {
+  console.log("click Right");
+
+  for (i = 0; i < imageSet.length; i++) {
+    imageSet.item(i).classList.remove("carousel-img-left");
+    imageSet.item(i).classList.add("carousel-img-right");
+    checkImageClassList(imageSet.item(i));
+  }
+}
+
+function checkImageClassList(element) {
+  console.log(element.classList);
+}
